db1ce76c6e35002198a206f8fcb1d5593f31a238
rishipatel9 <rishipatel0826@gmail.com>
2024-10-29 23:27:30 +0530

    Feat:Program Added

--- anchor/programs/votingapp/src/lib.rs
+++ anchor/programs/votingapp/src/lib.rs
@@ -1,7 +1,19 @@
 use anchor_lang::prelude::*;
 
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
-
+#[account]
+pub struct Poll {
+    pub poll_id: u64,
+    pub description: String, 
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
+#[account]
+pub struct Candidate{
+    pub candidate_name:String,
+    pub candidate_votes:u64
+}
 #[program]
 pub mod votingapp {
     use super::*;
@@ -20,16 +32,44 @@ pub mod votingapp {
             poll.candidate_amount=0;
         Ok(())
     } 
+
+    pub fn initialize_candidate(
+        ctx:Context<InitializeCandidate>,
+        candidate_name:String,
+        poll_id:u64
+    )  ->Result<()>{
+        let candidate=&mut ctx.accounts.candidate;
+        candidate.candidate_name=candidate_name,
+        candidate.candidate_votes=0;
+        Ok(())
+    }
 }
 
-#[account]
-pub struct Poll {
-    pub poll_id: u64,
-    pub description: String, 
-    pub poll_start: u64,
-    pub poll_end: u64,
-    pub candidate_amount: u64,
+
+#[derive(Accounts)]
+#[instruction(candidate_name:String,poll_id:u64)]
+pub struct InitializeCandidate<'info>{
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    #[account(
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 8 + 128 + 8 + 8 + 8,// Adjusted space
+        seeds = [poll_id.to_le_bytes().as_ref(),candidate_name.as_bytes()],
+        bump,
+    )]
+
+    pub candidate:Account<'info,Candidate>,
+     
+    pub system_program:Program<'info,System>,
 }
+
 
 #[derive(Accounts)]
 #[instruction(poll_id: u64)]
